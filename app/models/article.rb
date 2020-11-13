84b7fb65cad1a1b81823b3e716b78ec321154b89
madelinemc <madeline.coons@gmail.com>
2020-11-12 21:14:32 -0600

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,23 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+
+    # attr_accessor :title, :content
+
+    # @@all = []
+
+    # def initialize(params)
+    #     @title = title
+    #     @content = content
+    #     @@all << self
+    # end
+
+    # def self.all
+    #     @@all
+    # end
+
+    # def self.clear
+    #     @@all.clear
+    # end
+
 end
